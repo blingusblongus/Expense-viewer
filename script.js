dacfddcde2b5b6d31013dd1fdd14c69f2a48d176
Nick Allen <blingusblongus@users.noreply.github.com>
2017-05-11 14:17:32 -0500

    'no expenses' message now disappears when a new query is made, also, changed to run on form submit instead of just click

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-$('#submit').click(
-    function callGoogleScript() {
-        $('#working').html('fetching data...');
+$("form").submit(function(e) {
+    e.preventDefault();
+    callGoogleScript();
+});
 
-        var url = "https://script.google.com/macros/s/AKfycbwHgYtQkeRsqOYKcmWXjojmxortf4p7Rmc9a4549IHh/dev?callback=ctrlq&name=";
-        var name = $('#name').val();
-
-        var request = jQuery.ajax({
-            crossDomain: true,
-            url: url + encodeURIComponent(name),
-            method: "GET",
-            dataType: "jsonp"
-        });
-
-    });
-
-// print the returned data
-function ctrlq(e) {
+function callGoogleScript() {
+    $('#working').html('fetching data...');
     $('#table').html('');
-    $('#working').html('');
-    
-    console.log(e.result[0]);
 
-    if (e.result[0].date === "#N/A") {
-        $('#results').html('No expenses recorded this month');
-    } else {
-        for (var i in e.result) {
-            var obj = e.result[i];
-            $('#table').append(`<tr><td>${obj.date}</td><td>${obj.description}</td><td>${obj.budget}</td><td>${obj.amount}</td><td>${obj.paid}</td></tr>`);
+    var url = "https://script.google.com/macros/s/AKfycbwHgYtQkeRsqOYKcmWXjojmxortf4p7Rmc9a4549IHh/dev?callback=ctrlq&name=";
+    var name = $('#name').val();
+
+    var request = jQuery.ajax({
+        crossDomain: true,
+        url: url + encodeURIComponent(name),
+        method: "GET",
+        dataType: "jsonp"
+    });
+}
+
+    // print the returned data
+    function ctrlq(e) {
+        $('#working').html('');
+
+        if (e.result[0].date === "#N/A") {
+            $('#table').html('No expenses recorded this month');
+        } else {
+            for (var i in e.result) {
+                var obj = e.result[i];
+                $('#table').append(`<tr><td>${obj.date}</td><td>${obj.description}</td><td>${obj.budget}</td><td>${obj.amount}</td><td>${obj.paid}</td></tr>`);
+            }
         }
     }
-}
